4645d79aac2c89ae4cca1277ac0edea8a3c6cc99
diane <dianecronenwett@gmail.com>
2014-10-21 19:19:42 -0700

    Using UI Transform

--- canvas/ViewController.swift
+++ canvas/ViewController.swift
@@ -10,16 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var deadFace: UIImageView!
+    
+    @IBAction func deadButton(sender: AnyObject) {
+        
+        UIView.animateWithDuration(2, animations: { () -> Void in
+            self.deadFace.center.y += 270
+            self.deadFace.transform = CGAffineTransformMakeScale(2, 2)
+            }) { (finished: Bool) -> Void in
+             self.deadFace.transform = CGAffineTransformRotate(self.deadFace.transform, CGFloat(-10 * M_1_PI/180))
+        }
+    
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
