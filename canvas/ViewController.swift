242108a66de84e112dcadf20c1bb92bea55246ba
diane <dianecronenwett@gmail.com>
2014-10-23 19:39:13 -0700

    updated

--- canvas/ViewController.swift
+++ canvas/ViewController.swift
@@ -10,18 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var deadFace: UIImageView!
+    @IBOutlet var onDrag: UIPanGestureRecognizer!
+    @IBOutlet var imageView: UIView!
+    @IBOutlet weak var trayView: UIView!
+    @IBOutlet weak var downArrow: UIImageView!
+    var trayPosition: CGPoint!
+
     
-    @IBAction func deadButton(sender: AnyObject) {
-        
-        UIView.animateWithDuration(2, animations: { () -> Void in
-            self.deadFace.center.y += 270
-            self.deadFace.transform = CGAffineTransformMakeScale(2, 2)
-            }) { (finished: Bool) -> Void in
-             self.deadFace.transform = CGAffineTransformRotate(self.deadFace.transform, CGFloat(-10 * M_1_PI/180))
-        }
-    
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,10 +24,55 @@ class ViewController: UIViewController {
     }
     
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onPanTray(sender: AnyObject) {
+        var point = sender.locationInView(view)
+        var translation = sender.translationInView(view)
+        var velocity = sender.velocityInView(view)
+        
+        //trayView.frame.origin.y = point.y
+        
+        
+        
+       
+        if sender.state == UIGestureRecognizerState.Began {
+            println("Gesture began at: \(point)")
+            trayPosition = trayView.frame.origin
+            
+        } else if sender.state == UIGestureRecognizerState.Changed {
+            println("Gesture changed at: \(point)")
+            
+            var newTrayY = trayPosition.y + translation.y
+            if newTrayY < 347 {
+                trayView.frame.origin.y = (newTrayY - 347) * 0.1 + 347
+            }else {
+                trayView.frame.origin.y = newTrayY
+            }
+            
+            
+        } else if sender.state == UIGestureRecognizerState.Ended {
+            println("Gesture ended at: \(point)")
+            
+            UIView.animateWithDuration(0.2, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: nil, animations: { () -> Void in
+                
+                if velocity.y < 0 {
+                    self.trayView.frame.origin.y = 347
+                    self.downArrow.transform = CGAffineTransformIdentity
+                    
+                } else {
+                    self.downArrow.transform = CGAffineTransformMakeRotation(CGFloat(M_PI))
+                    self.trayView.frame.origin.y = 530
+                }
+                
+
+                }, completion: nil)
+           
+            
+        
+            
+        }
     }
+    
+    
     
     
 }
